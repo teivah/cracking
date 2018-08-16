62c3aa2ded04465f1c3710dc8c99322d9b663a38
teivah <teivah.io@gmail.com>
2018-08-17 00:03:33 +0200

    Notes

---
+++ src/io/teivah/bits/Thrice.java
@@ -0,0 +1,36 @@
+package io.teivah.bits;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Thrice {
+	public static void main(String[] args) {
+		new Thrice().main();
+	}
+
+	public void main() {
+		int n = singleNumber(Arrays.asList(1, 2, 4, 3, 3, 2, 2, 3, 1, 1));
+		System.out.println(n);
+	}
+
+	public int singleNumber(final List<Integer> list) {
+		int result = 0;
+
+		for (int i = 0; i < 32; i++) {
+			int n = 1 << i;
+
+			int count = 0;
+			for (int j = 0; j < list.size(); j++) {
+				if ((list.get(j) & n) != 0) {
+					count++;
+				}
+			}
+
+			if (count % 3 != 0) {
+				result |= n;
+			}
+		}
+
+		return result;
+	}
+}
