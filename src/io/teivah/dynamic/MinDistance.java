f8c87bcf01e367408d874d1eda1d07ea5712c6f2
teivah <teivah.io@gmail.com>
2018-08-21 22:54:34 +0200

    Notes

---
+++ src/io/teivah/dynamic/MinDistance.java
@@ -0,0 +1,19 @@
+package io.teivah.dynamic;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MinDistance {
+	public static void main(String[] args) {
+		new MinDistance().main();
+	}
+
+	public ArrayList<Integer> arrayList(Integer... n) {
+		return new ArrayList<>(Arrays.asList(n));
+	}
+
+	public void main() {
+	}
+
+}
